c825b278aa05b430edaf92b26f3f8a37a26b6322
corrodedHash <corrodedHash@users.noreply.github.com>
2017-06-30 11:45:37 +0200

    Worked on rotation

--- lsystem/lsystem.py
+++ lsystem/lsystem.py
@@ -1,4 +1,6 @@
 import literal_semantic
+import math
+
 class Lsystem:
     def __init__ (self, start_literal):
         self.start = start_literal
@@ -6,7 +8,15 @@ class Lsystem:
         self.rotation_stack = [(1, 0, 0)]
 
     def _move(self, terminal: literal_semantic.MoveTerminal):
-        self.position_stack[-1] += self.rotation_stack
+        self.position_stack[-1] += self.rotation_stack * terminal.distance
+    
+    def _rotate(self, terminal: literal_semantic.RotateTerminal):
+        new_x = self.position_stack[-1][0]
+        new_y = math.cos(terminal.rotation) - math.sin(teriminal.rotation)
+        new_z = math.sin(terminal.rotation) + math.cos(teriminal.rotation)
+        self.position_stack[-1] = (new_x, new_y, new_z)
+
+
 
     def get_vertices (self, level):
         for command in self.start.iterate(level):
