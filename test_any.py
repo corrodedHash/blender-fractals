62402eaf7aa595d1e50b7f4d57f2820356214ed1
corrodedHash <corrodedHash@users.noreply.github.com>
2017-07-10 00:17:01 +0200

    Made test_any work

--- test_any.py
+++ test_any.py
@@ -6,9 +6,12 @@ class TestSystem(unittest.TestCase):
 
     def test_general(self):
         x = None
-        with open("test_grammars/a.txt") as f:
+        with open("examples/dragon.txt") as f:
             x = lsystem.lsystem_parse.parse(f.read())
 
         self.assertTrue('A' in x._non_terminals)
+        self.assertTrue('X' in x._non_terminals)
+        self.assertTrue('Y' in x._non_terminals)
+
         self.assertEqual(x.start, x._non_terminals['A'])
-        self.assertEqual(x._non_terminals['A'].transition[0].distance, 2)
+        self.assertEqual(x._non_terminals['A'].transition[0].distance, 1)
