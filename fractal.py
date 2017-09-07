becd77fa54e30cfa98de87be50bba1b46221677d
corrodedHash <corrodedHash@users.noreply.github.com>
2017-09-07 12:22:24 +0200

    Added timing messages

--- fractal.py
+++ fractal.py
@@ -4,23 +4,33 @@ import bpy
 import bmesh
 
 from .fractal_cpp.fractal import x
+from .util.timer import Timer
 
 
 def _create_fractal(self, _context):
     if self.grammar_path == "":
         return
 
-    verts, edges = x(self.grammar_path, self.iteration)
+    with Timer(name="all", verbose=True):
+        with Timer(name="Generation", verbose=True):
+            verts, edges = x(self.grammar_path, self.iteration)
 
-    profile_mesh = bpy.data.meshes.new("FractalMesh")
-    profile_mesh.from_pydata(verts, edges, [])
-    profile_mesh.update()
-    profile_object = bpy.data.objects.new("Fractal", profile_mesh)
-    profile_object.data = profile_mesh
+        profile_mesh = bpy.data.meshes.new("FractalMesh")
 
-    scene = bpy.context.scene
-    scene.objects.link(profile_object)
-    profile_object.select = True
+        with Timer(name="Copying", verbose=True):
+            profile_mesh.vertices.add(len(verts)/3)
+            profile_mesh.vertices.foreach_set("co", verts)
+            profile_mesh.edges.add(len(edges)/2)
+            profile_mesh.edges.foreach_set("vertices_raw", edges)
+        #with Timer(name="Copying", verbose=True):
+        #    profile_mesh.from_pydata(verts, edges, [])
+        profile_mesh.update()
+        profile_object = bpy.data.objects.new("Fractal", profile_mesh)
+        profile_object.data = profile_mesh
+
+        scene = bpy.context.scene
+        scene.objects.link(profile_object)
+        profile_object.select = True
 
     # bpy.context.scene.objects.active = profile_object
     # bpy.ops.object.editmode_toogle()
