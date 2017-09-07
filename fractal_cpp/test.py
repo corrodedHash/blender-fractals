1e300e14ea198d344e4619579a2c38007c29218a
corrodedHash <corrodedHash@users.noreply.github.com>
2017-09-07 15:04:42 +0200

    Corrected files

--- fractal_cpp/test.py
+++
@@ -1,8 +0,0 @@
-import fractal
-v, e = fractal.x("grammars/bla.txt", 12)
-
-#for x in v:
-#    print(" ".join([str(x[j]) for j in range(3)]))
-#print("ayyy")
-#for x in e:
-#    print(" ".join([str(x[j]) for j in range(2)]))
