b1f1790a3fa3a57226705bdd860ae4e157e99030
corrodedHash <corrodedHash@users.noreply.github.com>
2017-09-07 15:03:24 +0200

    Removed obsolete files

--- fractal_cpp/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "fractalgen.h"
-#include <string>
-int main(int argc, char** args){
-  //mesh_info<double> result = generateMesh("grammars/test.grammar", 1);
-  mesh_info<double> result = generateMesh("../examples/standard/sierpinski.txt", std::stoi(std::string(args[1])));
-  return 0;
-}
