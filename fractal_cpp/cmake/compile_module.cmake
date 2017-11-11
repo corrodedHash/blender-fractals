74ae95dec3e8527cdc8f0dd38983f66027de8246
corrodedHash <corrodedHash@users.noreply.github.com>
2017-11-11 22:18:07 +0100

    Improved build system

--- fractal_cpp/cmake/compile_module.cmake
+++ fractal_cpp/cmake/compile_module.cmake
@@ -1,8 +1,6 @@
-add_library(fractalgen SHARED ${LIB_SRC_FILES} $<TARGET_OBJECTS:python_bridge> $<TARGET_OBJECTS:parsing>)
+add_library(fractalgen SHARED $<TARGET_OBJECTS:aux_python_bridge>)
 
-target_link_libraries(fractalgen python3.6m libantlr4-runtime.a)
-
-add_dependencies(fractalgen antlr4cpp_generation_antlr)
+target_link_libraries(fractalgen python3.6m corelib)
 
 target_compile_options(fractalgen PUBLIC -pthread -Wno-unused-result -Wsign-compare -DNDEBUG -g
   -fwrapv -O3 -Wall -march=x86-64 -mtune=generic -O2 -pipe
