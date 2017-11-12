742743e450454911480432345fce425bc4b27177
corrodedHash <corrodedHash@users.noreply.github.com>
2017-11-12 16:34:35 +0100

    Worked on benchmarking

--- fractal_cpp/benchmarks/whole.cpp
+++ fractal_cpp/benchmarks/whole.cpp
@@ -1,11 +1,12 @@
 #include "generator/fractalgen.h"
 #include <benchmark/benchmark.h>
 
+
 static void SierpinskiArgs(benchmark::internal::Benchmark* b)
 {
-  std::vector<int> runs {2, 4, 8, 10, 12, 14};
-  for (auto& level: runs)
-      b->Args({level});
+  std::vector<int> runs{ 2, 4, 8, 10, 12, 14 };
+  for (auto& level : runs)
+    b->Args({ level });
 }
 
 static void BM_Sierpinski(benchmark::State& state)
@@ -15,11 +16,18 @@ static void BM_Sierpinski(benchmark::State& state)
 }
 BENCHMARK(BM_Sierpinski)->Apply(SierpinskiArgs)->Unit(benchmark::kMicrosecond);
 
-
 static void BM_Menge(benchmark::State& state)
 {
   for (auto _ : state)
     generateFractal("/home/lukas/documents/coding/python/blender-fractals/examples/3d/menge.txt", state.range(0));
 }
 
-BENCHMARK(BM_Menge)->Args({1})->Args({2})->Args({3})->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_Menge)->Args({ 1 })->Args({ 2 })->Args({ 3 })->Unit(benchmark::kMicrosecond);
+
+static void BM_Dragon(benchmark::State& state)
+{
+  for (auto _ : state)
+    generateFractal("/home/lukas/documents/coding/python/blender-fractals/examples/standard/dragon.txt", state.range(0));
+}
+
+BENCHMARK(BM_Dragon)->Range(2, 14)->Unit(benchmark::kMicrosecond);
