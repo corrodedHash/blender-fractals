f9e0deed7ed193fd7ee10a6cfe6c52d63a0b218d
corrodedHash <corrodedHash@users.noreply.github.com>
2017-11-12 22:58:18 +0100

    Linting, testing, reverted axis_rotate

--- fractal_cpp/tests/fractalgen.cpp
+++ fractal_cpp/tests/fractalgen.cpp
@@ -1,6 +1,6 @@
+#include "generator/fractalgen.h"
 #include "catch.hpp"
 #include "valarray_comp.h"
-#include "generator/fractalgen.h"
 
 template <typename U>
 class FractalGenTester {
@@ -8,7 +8,7 @@ class FractalGenTester {
   frac::FractalGen<U> fractalgen;
   std::stack<std::valarray<U>>& get_pos() { return fractalgen.position_stack; };
   std::stack<std::valarray<U>>& get_rot() { return fractalgen.rotation_stack; };
-  frac::Fractal<U>& get_fractal() {return fractalgen.fractal;};
+  frac::Fractal<U>& get_fractal() { return fractalgen.fractal; };
   //std::stack<std::valarray<U>>& get_look() { return look_at_stack; };
   //std::stack<std::size_t>& get_verts() { return verts_stack; };
 };
@@ -16,56 +16,50 @@ class FractalGenTester {
 TEST_CASE("sierpinsky")
 {
   FractalGenTester<double> myfrac;
-  CHECK_THAT(myfrac.get_pos().top(), ValComp( std::valarray<double>({0, 0, 0}) ));
+  CHECK_THAT(std::valarray<double>({ 0, 0, 0 }), ValComp(myfrac.get_pos().top()));
 
-  myfrac.fractalgen.rotate({-60, 0, 0});
+  myfrac.fractalgen.rotate({ -60, 0, 0 });
   myfrac.fractalgen.draw(0.1);
-  CHECK_THAT(myfrac.get_pos().top(), ValComp( std::valarray<double>({0.05, 0, 0.086603}) ));
+  CHECK_THAT(std::valarray<double>({ 0.05, 0, 0.086603 }), ValComp(myfrac.get_pos().top()));
 
-  myfrac.fractalgen.rotate({60, 0, 0});
+  myfrac.fractalgen.rotate({ 60, 0, 0 });
   myfrac.fractalgen.draw(0.1);
-  CHECK_THAT(myfrac.get_pos().top(), ValComp( std::valarray<double>({0.15, 0, 0.086603}) ));
+  CHECK_THAT(std::valarray<double>({ 0.15, 0, 0.086603 }), ValComp(myfrac.get_pos().top()));
 
-  myfrac.fractalgen.rotate({60, 0, 0});
+  myfrac.fractalgen.rotate({ 60, 0, 0 });
   myfrac.fractalgen.draw(0.1);
-  CHECK_THAT(myfrac.get_pos().top(), ValComp( std::valarray<double>({0.2, 0, 0}) ));
+  CHECK_THAT(std::valarray<double>({ 0.2, 0, 0 }), ValComp(myfrac.get_pos().top()));
 }
 
-TEST_CASE("flikflak"){
+TEST_CASE("flikflak")
+{
   FractalGenTester<double> myfrac;
-  REQUIRE(myfrac.get_rot().top()[0] == 1);
-  REQUIRE(myfrac.get_rot().top()[1] == 0);
-  REQUIRE(myfrac.get_rot().top()[2] == 0);
+  CHECK_THAT(std::valarray<double>({ 1, 0, 0 }), ValComp(myfrac.get_rot().top()));
 
-  myfrac.fractalgen.rotate({60, 0, 0});
-  myfrac.fractalgen.rotate({-60, 0, 0});
-  myfrac.fractalgen.rotate({60, 0, 0});
-  myfrac.fractalgen.rotate({-60, 0, 0});
-  myfrac.fractalgen.rotate({60, 0, 0});
-  myfrac.fractalgen.rotate({-60, 0, 0});
+  myfrac.fractalgen.rotate({ 60, 0, 0 });
+  myfrac.fractalgen.rotate({ -60, 0, 0 });
+  myfrac.fractalgen.rotate({ 60, 0, 0 });
+  myfrac.fractalgen.rotate({ -60, 0, 0 });
+  myfrac.fractalgen.rotate({ 60, 0, 0 });
+  myfrac.fractalgen.rotate({ -60, 0, 0 });
 
-  REQUIRE(myfrac.get_rot().top()[0] == Approx(1));
-  REQUIRE(myfrac.get_rot().top()[1] == 0);
-  REQUIRE(myfrac.get_rot().top()[2] == Approx(0).margin(0.01));
+  CHECK_THAT(std::valarray<double>({ 1, 0, 0 }), ValComp(myfrac.get_rot().top()));
 }
 
-TEST_CASE("circle rot"){
+TEST_CASE("circle rot")
+{
   FractalGenTester<double> myfrac;
-  REQUIRE(myfrac.get_rot().top()[0] == 1);
-  REQUIRE(myfrac.get_rot().top()[1] == 0);
-  REQUIRE(myfrac.get_rot().top()[2] == 0);
+  CHECK_THAT(std::valarray<double>({ 1, 0, 0 }), ValComp(myfrac.get_rot().top()));
 
-  myfrac.fractalgen.rotate({60, 0, 0});
-  myfrac.fractalgen.rotate({60, 0, 0});
-  myfrac.fractalgen.rotate({60, 0, 0});
-  REQUIRE(myfrac.get_rot().top()[0] == Approx(-1));
-  REQUIRE(myfrac.get_rot().top()[1] == 0);
-  REQUIRE(myfrac.get_rot().top()[2] == Approx(0).margin(0.01));
+  myfrac.fractalgen.rotate({ 60, 0, 0 });
+  myfrac.fractalgen.rotate({ 60, 0, 0 });
+  myfrac.fractalgen.rotate({ 60, 0, 0 });
 
-  myfrac.fractalgen.rotate({60, 0, 0});
-  myfrac.fractalgen.rotate({60, 0, 0});
-  myfrac.fractalgen.rotate({60, 0, 0});
-  REQUIRE(myfrac.get_rot().top()[0] == Approx(1));
-  REQUIRE(myfrac.get_rot().top()[1] == 0);
-  REQUIRE(myfrac.get_rot().top()[2] == Approx(0).margin(0.01));
+  CHECK_THAT(std::valarray<double>({ -1, 0, 0 }), ValComp(myfrac.get_rot().top()));
+
+  myfrac.fractalgen.rotate({ 60, 0, 0 });
+  myfrac.fractalgen.rotate({ 60, 0, 0 });
+  myfrac.fractalgen.rotate({ 60, 0, 0 });
+
+  CHECK_THAT(std::valarray<double>({ 1, 0, 0 }), ValComp(myfrac.get_rot().top()));
 }
