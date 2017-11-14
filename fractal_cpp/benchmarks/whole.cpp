4b1f58f4afc958b78e2c8cb7557cb00963fa9165
corrodedHash <corrodedHash@users.noreply.github.com>
2017-11-14 14:48:10 +0100

    Worked on benchmarks

--- fractal_cpp/benchmarks/whole.cpp
+++ fractal_cpp/benchmarks/whole.cpp
@@ -1,21 +1,13 @@
 #include "generator/fractalgen.h"
 #include <benchmark/benchmark.h>
 
-static void SierpinskiArgs(benchmark::internal::Benchmark* b)
-{
-  std::vector<int> runs{ 2, 4, 8, 10, 12, 14 };
-  for (auto& level : runs) {
-    b->Args({ level });
-  }
-}
-
 static void BM_Sierpinski(benchmark::State& state)
 {
   for (auto _ : state) {
     frac::generateFractal("/home/lukas/documents/coding/python/blender-fractals/examples/standard/sierpinski.txt", state.range(0));
   }
 }
-BENCHMARK(BM_Sierpinski)->Apply(SierpinskiArgs)->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_Sierpinski)->Range(2, 14)->Unit(benchmark::kMicrosecond);
 
 static void BM_Menge(benchmark::State& state)
 {
