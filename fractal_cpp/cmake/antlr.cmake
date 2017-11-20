9bbb5fc21f64e1721d6937dd6e3938adc82e3689
corrodedHash <corrodedHash@users.noreply.github.com>
2017-11-20 07:59:29 +0100

    Improved build system

--- fractal_cpp/cmake/antlr.cmake
+++
@@ -1,7 +0,0 @@
-include( "${CMAKE_SOURCE_DIR}/cmake/ExternalAntlr4Cpp.cmake" )
-include_directories(${ANTLR4CPP_INCLUDE_DIRS})
-link_directories( ${ANTLR4CPP_LIBS} )
-antlr4cpp_process_grammar(fractal_parse antlr
-  ${CMAKE_SOURCE_DIR}/auxiliary/parsing/lsystemLexer.g4
-  ${CMAKE_SOURCE_DIR}/auxiliary/parsing/lsystemParser.g4)
-include_directories(${antlr4cpp_include_dirs_antlr})
