8239e550159c23422e96ff338781fe2ac9fa993f
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2019-09-08 17:45:44 +0200

    Update to 2.80

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Fractals",
     "author": "corrodedHash",
     "version": (0, 3),
-    "blender": (2, 75, 0),
+    "blender": (2, 80, 0),
     "location": "View3D > Add > Mesh > Fractal",
     "description": "Adds a new fractal",
     "warning": "",
@@ -10,43 +10,25 @@ bl_info = {
     "category": "Add Mesh",
 }
 
+if "bpy" in locals():
+    import importlib
+
+    importlib.reload(fractal)
+
+else:
+    from . import fractal
+
 import bpy  # NOQA
-
-# import dragon  # NOQA
-from .fractal import Fractal_add_object  # NOQA
-
-# Registration
-
-def add_fractal_button(self, context):
-    """Adds the fractal button"""
-    self.layout.operator(
-        Fractal_add_object.bl_idname,
-        text="Fractal",
-        icon='PLUGIN')
-
-
-# This allows you to right click on a button and link to the manual
-def add_dragon_manual_map():
-    """I don't know, does stuff"""
-    url_manual_prefix = "http://wiki.blender.org/index.php/Doc:2.6/Manual/"
-    url_manual_mapping = (
-        ("bpy.ops.mesh.add_object", "Modeling/Objects"),
-    )
-    return url_manual_prefix, url_manual_mapping
 
 
 def register():
     """Appends the fractal button to the menu"""
-    bpy.utils.register_class(Fractal_add_object)
-    # bpy.utils.register_manual_map(add_object_manual_map)
-    bpy.types.INFO_MT_mesh_add.append(add_fractal_button)
+    fractal.register()
 
 
 def unregister():
     """Removes the fractal button from the menu"""
-    bpy.utils.unregister_class(Fractal_add_object)
-    # bpy.utils.unregister_manual_map(add_object_manual_map)
-    bpy.types.INFO_MT_mesh_add.remove(add_fractal_button)
+    fractal.unregister()
 
 
 if __name__ == "__main__":
--- fractal.py
+++ fractal.py
@@ -1,122 +1,113 @@
 """Uses the cpp fractal generator"""
-import os
 import faulthandler
+import os
 
 import bpy
-from bpy_extras.io_utils import unpack_list, unpack_face_list
+import bpy_extras
 
 from .fractal_cpp.build.fractalgen import generate_fractal
 from .util.timer import Timer
 
 
-
-def _create_fractal_fast(self, _context):
+def _create_fractal(properties, context):
     faulthandler.enable()
-    if self.grammar_path == "":
+    if properties.grammar_path == "":
         return
 
     with Timer(name="all", verbose=True):
         with Timer(name="Generation", verbose=True):
-            verts, edges, faces = generate_fractal(self.grammar_path, self.iteration)
+            verts, edges, faces = generate_fractal(
+                properties.grammar_path, properties.iteration
+            )
             if edges is None:
                 edges = []
             if faces is None:
                 faces = []
 
-        len_faces = len(faces)
+        mesh = bpy.data.meshes.new("FractalMesh")
 
         with Timer(name="Copying", verbose=True):
-            me = bpy.data.meshes.new('Cube')
-            me.vertices.add(len(verts))
-            me.vertices.foreach_set("co", unpack_list(verts))
+            mesh.from_pydata(verts, edges, faces)
 
-            me.loops.add(len(faces.vertices))
-            me.loops.foreach_set("vertex_index", unpack_face_list(faces))
-
-            me.polygons.add(len(faces)) 
-            me.polygons.foreach_set("loop_start", list(faces.starts))
-            me.polygons.foreach_set("loop_total", list(faces.totals))
-#me.polygons.foreach_set("vertices", unpack_face_list(faces))
-        with Timer(name="Optimizing", verbose=True):
-
-            me.update(calc_edges=True)
-            me.calc_normals()
-#me.validate(True)
-
-        ob = bpy.data.objects.new(me.name, me)
-        bpy.context.scene.objects.link(ob)
-        bpy.context.scene.update()
-
-def _create_fractal(self, _context):
-    faulthandler.enable()
-    if self.grammar_path == "":
-        return
-
-    with Timer(name="all", verbose=True):
-        with Timer(name="Generation", verbose=True):
-            verts, edges, faces = generate_fractal(self.grammar_path, self.iteration)
-            if edges is None:
-                edges = []
-            if faces is None:
-                faces = []
-
-
-        profile_mesh = bpy.data.meshes.new("FractalMesh")
-
-        with Timer(name="Copying", verbose=True):
-            profile_mesh.from_pydata(verts, edges, faces)
-            #profile_mesh.vertices.foreach_get("co", verts)
-            #profile_mesh.edges.foreach_get("vertices", edges)
-
-        profile_mesh.update()
-        profile_object = bpy.data.objects.new("Fractal", profile_mesh)
-        profile_object.data = profile_mesh
-
-        scene = bpy.context.scene
-        scene.objects.link(profile_object)
+        mesh_obj = bpy_extras.object_utils.object_data_add(context, mesh, operator=properties)
 
         with Timer(name="Optimizing", verbose=True):
-            scene.objects.active = profile_object
+            context.view_layer.objects.active = mesh_obj
+            mesh_obj.select_set(True)
 
-            bpy.ops.object.mode_set(mode='EDIT')
+            bpy.ops.object.mode_set(mode="EDIT")
             bpy.ops.mesh.remove_doubles(threshold=0.0001)
-            bpy.ops.object.mode_set(mode='OBJECT')
+            bpy.ops.object.mode_set(mode="OBJECT")
 
     # bpy.context.window_manager.progress_end()
 
 
-class Fractal_add_object(bpy.types.Operator):
+class add_mesh_fractal(bpy.types.Operator, bpy_extras.object_utils.AddObjectHelper):
     """Create a new Fractal"""
+
     bl_idname = "mesh.add_fractal"
     bl_label = "Add Fracal"
-    bl_options = {'REGISTER', 'UNDO'}
+    bl_options = {"REGISTER", "UNDO"}
 
-    iteration = bpy.props.IntProperty(
+    iteration: bpy.props.IntProperty(
         name="Iteration Count",
         default=1,
         min=1,
         soft_min=1,
         soft_max=10,
-        subtype='UNSIGNED',
-        description="Number of iterations of the fractal")
+        subtype="UNSIGNED",
+        description="Number of iterations of the fractal",
+    )
 
-    standard_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                 "examples", "3d", "menge.txt")
+    standard_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "examples", "3d", "menge.txt"
+    )
 
     def reset_iteration(self, _context):
         """Resets iteration"""
         self.iteration = 2
 
-    grammar_path = bpy.props.StringProperty(
+    grammar_path: bpy.props.StringProperty(
         name="Grammar path",
         default=standard_path,
         description="The grammar for the fractal you want to draw",
-        subtype='FILE_PATH',
-        update=reset_iteration
+        subtype="FILE_PATH",
+        update=reset_iteration,
     )
 
     def execute(self, context):
         """Create the fractal"""
         _create_fractal(self, context)
 
-        return {'FINISHED'}
+        return {"FINISHED"}
+
+    def invoke(self, context, event):
+        """Invoke"""
+        self.execute(context)
+        return {"FINISHED"}
+
+
+# Register:
+def menu_func_fractal(self, context):
+    layout = self.layout
+    layout.separator()
+    self.layout.operator(add_mesh_fractal.bl_idname, text="Fractal", icon="MOD_TIME")
+
+
+classes = (add_mesh_fractal,)
+
+
+def register():
+    from bpy.utils import register_class
+
+    for cls in classes:
+        register_class(cls)
+    bpy.types.VIEW3D_MT_mesh_add.append(menu_func_fractal)
+
+
+def unregister():
+    from bpy.utils import unregister_class
+
+    for cls in reversed(classes):
+        unregister_class(cls)
+    bpy.types.VIEW3D_MT_mesh_add.remove(menu_func_fractal)
